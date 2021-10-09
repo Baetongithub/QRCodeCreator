967980409e20993762ddee53f8eea2a107b08d63
Baet <toktonbekovbaet@gmail.com>
2021-10-09 23:11:15 +0600

    alpha

--- app/src/main/java/com/geektech/qrcodecreator/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/geektech/qrcodecreator/ui/fragments/HomeFragment.kt
@@ -1,38 +1,51 @@
 package com.geektech.qrcodecreator.ui.fragments
 
+import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.Toolbar
+import androidx.activity.result.contract.ActivityResultContracts
 import com.geektech.qrcodecreator.R
 import com.geektech.qrcodecreator.databinding.FragmentHomeBinding
 import com.geektech.qrcodecreator.extensions.getDrawable
+import com.geektech.qrcodecreator.extensions.toast
 import com.geektech.qrcodecreator.ui.base.BaseFragment
 import com.geektech.qrcodecreator.utils.GenerateCode
 import com.geektech.qrcodecreator.utils.PrintHelp
 import com.geektech.qrcodecreator.utils.SavePhotoToStorage
 import com.geektech.qrcodecreator.utils.ShareImage
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+
     override fun viewBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentHomeBinding =
         FragmentHomeBinding.inflate(inflater, container, false)
 
-    //    private fun scanQRCodeButton() {
-//        val integrator = IntentIntegrator(requireActivity())
-//        integrator.initiateScan()
-//    }
-//
-////    val intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-////    if (intentResult.contents != null) {
-////        vb.etValue.setText(intentResult.contents)
-////    }
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult?.resultCode == Activity.RESULT_OK) {
+                val intentResult: IntentResult =
+                    IntentIntegrator.parseActivityResult(activityResult.resultCode, activityResult.data)
+                if (intentResult.contents != null)
+                    toast(intentResult.contents)
+                    vb.etUrl.setText(intentResult.contents)
+            }
+        }
 
     override fun setupUI() {
 
         val toolbar: Toolbar? = activity?.findViewById(R.id.toolbar_custom)
         activity?.setActionBar(toolbar)
+
+        vb.imageScanQrCode.setOnClickListener {
+            val integrator = IntentIntegrator(activity)
+            integrator.initiateScan()
+           // getContent.launch(null)
+        }
 
         initClicks()
         onClickButtons()
---
+++ app/src/main/java/com/geektech/qrcodecreator/utils/ResultLauncher.kt
@@ -0,0 +1,34 @@
+package com.geektech.qrcodecreator.utils
+
+import android.app.Activity
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.LifecycleOwner
+import com.google.zxing.integration.android.IntentIntegrator
+
+class ResultLauncher(
+    private val activity: Activity?,
+    lifecycleOwner: LifecycleOwner,
+    activityResultRegistry: ActivityResultRegistry,
+    callback: (resultCode: ActivityResult) -> Unit,
+) {
+
+    private val registerActivityResult =
+        activityResultRegistry.register(
+            REGISTRY_KEY,
+            lifecycleOwner,
+            ActivityResultContracts.StartActivityForResult(),
+            callback
+        )
+
+    fun scanQRCodeButton() {
+        val integrator = IntentIntegrator(activity)
+//        integrator.initiateScan()
+        registerActivityResult.launch(integrator.createScanIntent())
+    }
+
+    private companion object {
+        private const val REGISTRY_KEY = "intent_settings"
+    }
+}
